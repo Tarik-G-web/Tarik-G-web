e3f780542c0d651508c5276f7638201f9c83b782
Tarik-G-web <tarikg19922004.com>
2024-11-27 10:41:44 -0800

    initial commit

---
+++ constant/index.ts
@@ -0,0 +1,30 @@
+export const LINKS = [
+ { 
+     title: 'home',
+     path: 'home',
+     offset: -50,
+ },
+ {
+    title: 'feature',
+    path: 'feature',
+    offset: -50,
+ },
+ {
+    title: 'listing',
+    path: 'listing',
+    offset: -50,
+ },
+ {
+    title: 'testimonials',
+    path: 'testimonials',
+    offset: -50,
+ },
+ {
+    title: 'downloads',
+    path: 'downloads',
+    offset: -50,
+
+ },
+
+
+]
